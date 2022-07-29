15f43f390956895361238a30d0fc6700da38b172
Noborita9 <joaquinbonora9@gmail.com>
2022-07-29 13:36:56 -0300

    start database connection script

--- scripts/taskDAO.py
+++ scripts/taskDAO.py
@@ -1,6 +1,21 @@
+import sqlite3 as sql
 class TaskDAO():
     def __init__(self):
         pass
 
-    def connect(self, url: str):
-        pass
+    def connect(self, db_name: str):
+        connection = sql.connect(db_name)
+        cursor = connection.cursor()
+        return cursor
+
+    def create_table(self, cursor: sql.Cursor, table_name: str, item_dict: dict):
+        state = {
+                "table_created" : False,
+                "err" : None,
+                }
+        try:
+            cursor.execute(table_name)
+        except Exception:
+            pass
+        return state
+

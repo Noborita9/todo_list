773fe7e6f13924afc5189c2b99ee1467c7c52aa6
noborita9 <joaquinbonora9@gmail.com>
2022-07-31 12:07:39 -0300

    remove useless code

--- scripts/task.py
+++ scripts/task.py
@@ -2,7 +2,13 @@ import datetime
 
 
 class Task():
-    def __init__(self, id: int, title: str, desc: str, urgency: float, date_init: datetime.date = datetime.date.today(), date_end: datetime.date = datetime.date.today(), completed: bool = False) -> None:
+    def __init__(self, id: int,
+                 title: str,
+                 desc: str, 
+                 urgency: float,
+                 date_init: datetime.date = datetime.date.today(),
+                 date_end: datetime.date = datetime.date.today(),
+                 completed: bool = False) -> None:
         self.id = id
         self.title = title
         self.desc = desc
@@ -10,30 +16,3 @@ class Task():
         self.date_init = date_init
         self.date_end = date_end
         self.completed = completed
-
-    def set_title(self, new_title):
-        self.title = new_title
-
-    def set_text(self, new_text):
-        self.text = new_text
-
-    def set_urgency(self, urgency_level):
-        self.urgency = urgency_level
-
-    def set_date_end(self, end_date):
-        self.date_end = end_date
-
-    def print_task(self):
-        print(f"{self.title}:   urgency: {self.urgency}\n{self.text}\ninitialized: {self.date_init} | finalization: {self.date_end}")
-
-
-def main():
-    task_1 = Task(1, "Title", "Text", 0.5)
-    task_1.set_date_end(datetime.date(2022, 12, 28))
-    task_1.print_task()
-    print(type(task_1.date_init))
-    print(type(task_1.date_end))
-
-
-if __name__ == "__main__":
-    main()
